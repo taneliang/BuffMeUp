71829cc960b4e69ee7fa3b36707079f1a1d23613
unknown <xiaoichiban@hotmail.com>
2018-01-27 22:19:55 +0800

    Update time on current buffet

--- src/BuffetDetail.vue
+++ src/BuffetDetail.vue
@@ -1,10 +1,3 @@
-<style>
-  p {
-    colour: blue;
-    padding-right: 100px;
-  }
-</style>
-
 <template>
   <div id="buffet">
     <h3>Buffet Name: {{ buffet.description }}</h3>
@@ -12,7 +5,7 @@
     <p>Is food halal: {{buffet.halal}}</p>
     <p>Amount of food left (0-1): {{buffet.foodremaining}}</p>
     <p>Buffet still available: {{buffet.open}}</p>
-    <p>Clearing time: {{buffet.time}}</p>
+    <p>Clearing time: {{buffet.time.toLocaleString()}}</p>
   </div>
 </template>
 
