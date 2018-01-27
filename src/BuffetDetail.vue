c17be090b8185daa279eb08fd8ba630efa305d44
E-Liang Tan <eliang@eliangtan.com>
2018-01-27 21:17:49 +0800

    Implement buffet submission form

--- src/BuffetDetail.vue
+++ src/BuffetDetail.vue
@@ -7,6 +7,6 @@
 <script>
 export default {
   name: "buffet-detail",
-  props: ['buffet'],
+  props: ["buffet"]
 };
 </script>
--- src/SubmissionForm.vue
+++ src/SubmissionForm.vue
@@ -5,9 +5,10 @@
       <div>
           <label for="location">Location:</label>
           <input
-            v-model="message" id="location"
+            v-model="location"
+            id="location"
             name="buff_location"
-            placeholder = "E.g. Yusof Ishak House">
+            placeholder="E.g. Yusof Ishak House">
       </div>
       <div>
           <label for="halal">Buffet is Halal  </label>
@@ -15,42 +16,45 @@
             type="checkbox"
             id="halal"
             name="buff_halal"
-            v-model = "checked">
+            v-model="is_halal">
       </div>
       <div>
           <label for="desc">Description:</label>
-          <textarea v-model="buff_desc" placeholder=
+          <textarea v-model="desc" placeholder=
             "Description of the buffet i.e. Type of food available">
           </textarea>
-          <p>Description is: {{buff_desc}}</p>
 
       </div>
-      <button type="submit"> Submit </button>
+      <button type="submit" v-on:click="onSubmit"> Submit </button>
     </form>
-<!-- <p>Description is: {{buff_desc}}</p> -->
   </div>
 </template>
 
 <script>
-//import db from "./datab";
+import db from "./datab";
 
-// console.log(buff_desc);
-// db
-//   .collection("Buffets")
-//   .add({
-//     description: "AMAZING",
-//     halal: true,
-//     open: true,
-//     foodremaining: 0.5
-//   })
-//   .then(function(docRef) {
-//     console.log("Document successfully written! ID:", docRef.id);
-//   })
-//   .catch(function(error) {
-//     console.error("Error writing document: ", error);
-//   });
-//
 export default {
-  name: "subform"
+  name: "subform",
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      db
+        .collection("Buffets")
+        .add({
+          description: this.desc,
+          location: this.location,
+          halal: this.is_halal,
+          open: true,
+          foodremaining: 0.5,
+          time: new Date()
+        })
+        .then(function(docRef) {
+          console.log("Document successfully written! ID:", docRef.id);
+        })
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
+    }
+  }
 };
 </script>
