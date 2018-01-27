275418142469a09abc3bb3a9326dd071277c1c90
E-Liang Tan <eliang@eliangtan.com>
2018-01-27 21:26:38 +0800

    Revert "Merge branch 'master' of github.com:taneliang/hax"
    This reverts commit dab3be39698e9e03b9bc7fa5bd66789e10e8104c, reversing
    changes made to c17be090b8185daa279eb08fd8ba630efa305d44.

--- src/BuffetDetail.vue
+++ src/BuffetDetail.vue
@@ -1,18 +1,6 @@
-<style>
-  p {
-    colour: blue;
-    padding-right: 100px;
-  }
-</style>
-
 <template>
   <div id="buffet">
-    <h3>Buffet Name: {{ buffet.description }}</h3>
-    <p>Location: {{buffet.location}}</p>
-    <p>Is food halal: {{buffet.halal}}</p>
-    <p>Amount of food left (0-1): {{buffet.foodremaining}}</p>
-    <p>Buffet still available: {{buffet.open}}</p>
-    <p>Clearing time: {{buffet.time}}</p>
+    <h3>Buffet des {{ buffet.description }}</h3>
   </div>
 </template>
 
