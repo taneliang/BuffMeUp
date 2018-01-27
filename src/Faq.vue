488aec0298bed451669b182cdd8acbbc4a52b58f
unknown <xiaoichiban@hotmail.com>
2018-01-27 18:33:52 +0800

    Change spelling at FAQ

--- src/Faq.vue
+++ src/Faq.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="faq">
+    <!-- FAQ here -->
     <h2>FAQ</h2> </br>
-    <h3>Q:How do I locate the places in NUS and to get? </h3>
+    <h3>Q: How do I locate the places in NUS and to get? </h3>
       <p>A: Do take a look at this website to navigate:
     <a href="http://www.nus.edu.sg/oca/Transport/Getting-around-NUS.html#krc">HERE</a></p><br>
 
     <h3>Q: Is the food Halal? </h3>
-      <p>A: By default food is halah unless specified</p><br>
+      <p>A: By default food is halal unless specified</p><br>
 
     <h3>Q: What are the guidelines to post information about buffet? </h3>
       <p>A: 1. Get permission from organisers before posting <br>
@@ -15,7 +16,6 @@
             4. Buffet clearing time</p> <br>
     <h3>Q: Can I make clarifications of leftover food? </h3>
       <p>A: Yes do feel free to ask any question that concerns you!</p><br>
-    <!-- FAQ here -->
   </div>
 </template>
 
