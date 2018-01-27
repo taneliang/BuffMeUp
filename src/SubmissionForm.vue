1ad669d73dc662d478ed3af8ea21b74cb5e6307a
David Goh <35485082+Lessthanfree@users.noreply.github.com>
2018-01-27 23:15:06 +0800

    Add Timing drop-down selector. Remove slider for food remaining

--- src/SubmissionForm.vue
+++ src/SubmissionForm.vue
@@ -2,7 +2,7 @@
   <div id="subform">
     <h2> Submission Form </h2>  </br>
     <form>
-      <div>
+      <div id = "location">
           <label for="location">Location:</label>
           <input
             v-model="location"
@@ -10,7 +10,11 @@
             name="buff_location"
             placeholder="E.g. Yusof Ishak House">
       </div>
-      <div>
+      <div id = "foodremaining">
+        <label for="foodremaining">Amount of food remaining:</label>
+          <input type="text" v-model="foodrem">
+      </div>
+      <div id = "halal">
           <label for="halal">Buffet is Halal  </label>
           <input
             type="checkbox"
@@ -18,8 +22,17 @@
             name="buff_halal"
             v-model="is_halal">
       </div>
-      <div>
-          <label for="desc">Description:</label>
+      <div id = "expirytiming">
+        <label for="exptime">Expiry timing:</label>
+        <select name="exphrs" v-model = "exptimehrs">
+          <option v-for="time in ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']">{{time}}</option>
+        </select>
+        <select name="expmins" v-model = "exptimemins">
+          <option v-for="mins in ['00','15','30','45']">{{mins}}</option>
+        </select>
+      </div>
+      <div id = "description">
+          <label for="desc">Description:</label>a
           <textarea v-model="desc" placeholder=
             "Description of the buffet i.e. Type of food available">
           </textarea>
@@ -38,6 +51,8 @@ export default {
   methods: {
     onSubmit(e) {
       e.preventDefault();
+      const exphrs = parseInt(this.exptimehrs);
+      console.log(this.exptimehrs, exphrs, this.exptimemins, this.foodrem);
       db
         .collection("Buffets")
         .add({
@@ -45,7 +60,7 @@ export default {
           location: this.location,
           halal: this.is_halal,
           open: true,
-          foodremaining: 0.5,
+          foodremaining: this.foodrem,
           time: new Date()
         })
         .then(function(docRef) {
