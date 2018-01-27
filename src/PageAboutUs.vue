e6d4339f607fa0ae506ba5ee6d287aa4e6cc81e7
David Goh <35485082+Lessthanfree@users.noreply.github.com>
2018-01-28 07:14:11 +0800

    Add AboutUs page and router pathing

---
+++ src/PageAboutUs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="about">
+    <!-- FAQ here -->
+    <h2>About Us</h2> </br>
+    <h3>What is BuffMeUp? </h3>
+      <p>BuffMeUp is an initiative to reduce food wastage of buffets in Singapore
+      while filling the stomachs of peckish people.<br> It's a win-win situation.</p><br>
+
+    <h3>How does it work?</h3>
+      <p>If someone finds their buffet has way too much leftover food for them
+        to possibly consume by themeselves, they can make a post on BuffMeUp.<br>
+
+        Their buffet will then be visible on our "Today" page and potential
+        buffet helpers will be able to respond to the culinary call to arms.<br>
+
+        Once a buffet has been vanquished, a user will be able to celebrate the
+        victory by deleting the post so that the world may know of their great
+        accomplishment!<br>
+        (So other brave buffet helpers don't end up visiting an empty buffet)<br>
+        </p> <br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+};
+</script>
