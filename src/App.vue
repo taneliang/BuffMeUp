9d22780261e2a010cf5ec8143abbef1b19cdf60c
E-Liang Tan <eliang@eliangtan.com>
2018-01-27 17:13:30 +0800

    Implement FAQ shell

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
     <h1>Buffmeup</h1>
+    <faq></faq>
   </div>
 </template>
 
 <script>
+import Faq from './Faq.vue'
 export default {
   name: "app",
+  components: { Faq }
 };
 </script>
---
+++ src/Faq.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="faq">
+    <h2>FAQ</h2>
+    <!-- FAQ here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "faq",
+};
+</script>
