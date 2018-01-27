a2ceafcd7c9f20b735338b53cb9b4d6f64375fba
E-Liang Tan <eliang@eliangtan.com>
2018-01-27 20:00:01 +0800

    Add buffet detail shell

---
+++ src/BuffetDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="buffet">
+    <h3>Buffet des {{ buffet.description }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "buffet-detail",
+  props: ['buffet'],
+};
+</script>
--- src/Buffets.vue
+++ src/Buffets.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="buffets">
     <h2>===Available Buffets===</h2>
-    <!-- Buffets here -->
-    <p>{{ buffets.length }}</p>
+    <buffet-detail v-for="buffet in buffets" v-bind:buffet="buffet" v-bind:key="buffet.id"></buffet-detail>
   </div>
 </template>
 
 <script>
 import db from "./datab";
+import BuffetDetail from "./BuffetDetail.vue";
+
 export default {
   name: "buffets",
   data() {
@@ -20,6 +21,7 @@ export default {
     return {
       buffets: []
     };
-  }
+  },
+  components: { BuffetDetail }
 };
 </script>
