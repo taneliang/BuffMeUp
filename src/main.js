e24f7298148506cb4594e917a01e54fea63ecc56
David Goh <35485082+Lessthanfree@users.noreply.github.com>
2018-01-27 17:43:39 +0800

    Add Firebase/Firestore api
    Allows us to read the database. API address is missing btw

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import App from "./App.vue";
+import firebase from "firebase";
+import "@firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "https://haxaton.firebaseio.com/",
+  projectId: "haxaton"
+});
 
 new Vue({
   el: "#app",
